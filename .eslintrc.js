96d2ea52f88fed6f28cd07eb70e0f0c69a2cf1db
youyiqin <564151947@qq.com>
2021-06-29 00:13:37 +0800

    chore: :memo: improve configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,17 @@ module.exports = {
     es2021: true,
   },
   extends: [
+    'airbnb/hooks',
+    'plugin:eslint-comments/recommended',
+    'plugin:import/typescript',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'airbnb',
-    'airbnb/hooks',
+    'plugin:unicorn/recommended',
+    'prettier',
+    // 专门支持了 eslint-plugin-react
+    'prettier/react',
+    // 专门支持了 @typescript-eslint/eslint-plugin
+    'prettier/@typescript-eslint',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -17,12 +24,8 @@ module.exports = {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
-  rules: {
-  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
   settings: {
     'import/resolver': {
       node: {
