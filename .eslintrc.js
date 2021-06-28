2964fc1c17c934c55b240a5ca5722986d5c00d61
youyiqin <564151947@qq.com>
2021-06-28 23:34:44 +0800

    add some rules

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb/hooks',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        // 指定 eslint-plugin-import 解析的后缀名
+        extensions: ['.ts', '.tsx', '.js', '.json'],
+      },
+    },
+    'import/extensions': [
+      '2',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+        json: 'never',
+        js: 'never',
+      },
+    ],
+  },
+};
